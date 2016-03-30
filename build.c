b4ec737d61570b6e51573f2ac7aded537a839dc2
Jincheng Li <jl4569@columbia.edu>
2016-03-30 16:08:35 -0400

    Update build.c

--- build.c
+++ build.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+#include "tree.h"
+
 int main(int argc, char *argv[]) {
     if (argc < 4) {
         printf("usage: %s <num keys> <num probes> <list of fanout parameters...>", argv[0]);
     }
-    printf("building the tree\n");
+    printf("building the tree...\n");
     return 0;
 }
