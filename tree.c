e8357e5644c7ab1c44d3563a83193fdc45bee4e3
Jincheng Li <jl4569@columbia.edu>
2016-04-04 16:03:13 -0400

    refactor code to make it look nicer

--- tree.c
+++ tree.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <time.h>
+#include <stdbool.h>
 
 #include "tree.h"
 #include "random.h"
 
 // allocates memory aligned at 16-byte boundary
-#define ALIGNED_ALLOC(ptr, size) posix_memalign((void **) (&(ptr)), 16, size)
+#define ALIGNED_ALLOC(ptr, size) {                          \
+        if (posix_memalign((void **) (&(ptr)), 16, size)) { \
+            perror("posix_memalign");                       \
+            exit(EXIT_FAILURE);                             \
+        }                                                   \
+    }
 
+// maximum number of keys for a tree with given fanouts, at a particular level
 int32_t num_keys_at_level(size_t level, int32_t *fanouts) {
     assert(level >= 0 && "level should be non-negative");
 
@@ -21,84 +28,72 @@ int32_t num_keys_at_level(size_t level, int32_t *fanouts) {
     return n;
 }
 
+// maximum number of keys for a tree with given levels and fanouts
+// this is simply sum(num_keys_at_level(i) for each level i)
 int32_t max_num_keys(int32_t num_levels, int32_t *fanouts) {
     assert(num_levels >= 0 && "num_levels should be non-negative");
-
-    if (num_levels == 0)
-        return 0;
-    
-    return (fanouts[0]-1) + fanouts[0] * max_num_keys(num_levels-1, fanouts+1);
+    return (num_levels == 0) ? 0:
+        (fanouts[0]-1) + fanouts[0] * max_num_keys(num_levels-1, fanouts+1);
 }
 
+// minimum number of keys - root has one key, left-most child all filled
 int32_t min_num_keys(int32_t num_levels, int32_t *fanouts) {
     assert(num_levels > 0 && "num_levels should be positive");
-
-    // minimum: root has one key, left-most child all filled
     return max_num_keys(num_levels-1, fanouts+1) + 1;
 }
 
 void init_partition_tree(int32_t k, int32_t num_levels, int32_t *fanouts,
                          partition_tree *tree) {
     if (k > max_num_keys(num_levels, fanouts)) {
-        fprintf(stderr, "error: too many build keys for partition tree, maximum %d keys\n",
+        fprintf(stderr, "error: too many build keys, maximum %d keys\n",
                 max_num_keys(num_levels, fanouts));
         exit(EXIT_FAILURE);
     }
 
     if (k < min_num_keys(num_levels, fanouts)) {
-        fprintf(stderr, "error: too few build keys for partition tree, minimum %d keys\n",
+        fprintf(stderr, "error: too few build keys, minimum %d keys\n",
                 min_num_keys(num_levels, fanouts));
         exit(EXIT_FAILURE);
     }
 
     tree->num_levels = num_levels;
-    if (ALIGNED_ALLOC(tree->fanouts, sizeof(int32_t  ) * num_levels) ||
-        ALIGNED_ALLOC(tree->nodes,   sizeof(int32_t *) * num_levels)) {
-        perror("posix_memalign");
-        exit(EXIT_FAILURE);
-    }
+    ALIGNED_ALLOC(tree->fanouts, sizeof(int32_t  ) * num_levels);
+    ALIGNED_ALLOC(tree->nodes,   sizeof(int32_t *) * num_levels);
 
     size_t i;
     for (i = 0; i < num_levels; i++) {
         // allocate memory for each level of tree (represented as a single array)
         tree->fanouts[i] = fanouts[i];
-        if (ALIGNED_ALLOC(tree->nodes[i],
-                          sizeof(int32_t) * num_keys_at_level(i, fanouts))) {
-            perror("posix_memalign");
-            exit(EXIT_FAILURE);
-        }
+        ALIGNED_ALLOC(tree->nodes[i],
+                      sizeof(int32_t) * num_keys_at_level(i, fanouts));
     }
-    
 
     // populate the tree with randomly-generated integers
     rand32_t *gen = rand32_init(time(NULL));
     int32_t  *keys = generate_sorted_unique(k, gen);
     free(gen);
 
-    // tail at each level
-    size_t tails[num_levels];
-
-    // boolean indicating whether each level just had a node filled
-    int32_t filled[num_levels]; 
+    size_t tails[num_levels];  // tail at each level
+    bool   filled[num_levels]; // whether each level just had a node filled
 
     for (i = 0; i < num_levels; i++) {
         tails[i] = 0;
-        filled[i] = 0;
+        filled[i] = false;
     }
 
     int32_t j;
     for (i = 0; i < k; i++) {
         for (j = num_levels - 1; j >= 0; j--) {
             if (filled[j]) {
-                // go up one level and clear the filled bit
-                filled[j] = 0;
+                // go up one level and clear the filled bit at this level
+                filled[j] = false;
                 continue;
             } else {
                 // insert key at this level
                 tree->nodes[j][tails[j]] = keys[i];
                 tails[j]++;
                 if (tails[j] % (tree->fanouts[j] - 1) == 0) {
-                    filled[j] = 1;
+                    filled[j] = true;
                 }
                 break;
             }
